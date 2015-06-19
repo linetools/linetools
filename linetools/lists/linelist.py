95c48be9e09af51b7a5d1a9f7486e6dd5d5390a7
J. Xavier Prochaska <xavier@ucolick.org>
2015-06-19 13:05:56 -0700

    jxp

--- linetools/lists/linelist.py
+++ linetools/lists/linelist.py
@@ -7,13 +7,153 @@ import numpy as np
 import os
 
 from astropy import units as u
+from astropy.units.quantity import Quantity
 from astropy import constants as const
 from astropy.io import fits
+from astropy.table import QTable, Table, vstack
 
-#from xastropy.xutils import xdebug as xdb
+from xastropy.xutils import xdebug as xdb
 
 #
-class LineList(Spectrum1D):
+class LineList(object):
     '''Class to over-load Spectrum1D for new functionality not yet in specutils
-    '''
 
+    Parameters:
+    ----------
+    llst_keys: str or list  
+      Input to grab line list.  Current options are:
+       'ISM'     :: "All" ISM lines
+       'Strong'  :: Strong ISM lines
+       'HI'      :: HI Lyman series
+       'H2'      :: H2 (Lyman-Werner)
+       'CO'      :: CO UV band-heads [not yet implemented]
+       'EUV'     :: Key EUV lines (for CASBAH project)
+       'Gal_E'   :: Galaxy emission lines (HII)
+       'Gal_A'   :: Galaxy absorption lines (stellar)
+       'AGN'     :: Key AGN lines
+    '''
+    # Init
+    def __init__(self, llst_keys):
+
+        # Error catching
+        if type(llst_keys) not in [str,list]:
+            raise TypeError('LineList__init__: Wrong type for LineList input')
+
+        # Save
+        if type(llst_keys) in [str]:
+            self.lists = [llst_keys]
+        else:
+            self.lists = llst_keys
+
+        # Load Data
+        self.load_data()
+
+        # Set lines for use
+        self.set_lines()
+
+    # 
+    def load_data(self):
+        ''' Grab the data for the lines of interest
+        '''
+        # Import
+        from linetools.lists import parse as lilp
+        reload(lilp)
+
+        # Define datasets
+        dataset = {
+            'ism': [None], 
+            'molecules': [lilp.read_H2]   # H2 
+            }
+
+        # Loop on lists
+        for llist in self.lists:
+            if llist == 'H2':
+                sets = ['molecules']
+            else:
+                raise ValueError('Not ready for this group')
+
+        full_table = None
+        all_func = []
+        # Loop on data sets
+        for iset in sets:
+            # Loop on data sources
+            for func in dataset[iset]:
+                # Query if read already
+                if func not in all_func:
+                    # Read
+                    table = func()
+                    if full_table is None:
+                        full_table = table
+                    else:
+                        full_table = vstack([full_table, table])
+                    # Save to avoid repeating
+                    all_func.append(func)
+
+        # Save as QTable
+        self._fulltable = QTable(full_table)
+
+    #####
+    def set_lines(self):
+        ''' Parse the lines of interest
+        '''
+        # Loop on lines
+        indices = []
+        for llist in self.lists:
+            if llist == 'H2':
+                gdi = np.where(self._fulltable['mol'] == 'H2')[0]
+                if len(gdi) == 0:
+                    raise IndexError('set_lines: Found no H2 molecules! Read more data')
+                indices.append(gdi)
+
+        # Collate (should grab unique ones!)
+        all_idx = np.concatenate( [np.array(itt) for itt in indices] )
+
+        # Parse (consider masking instead)
+        self._data = self._fulltable[all_idx]
+
+    #####
+    def __getattr__(self,k):
+        ''' Passback an array or Column of the data 
+        k must be a Column name in the data Table
+        '''
+        return self._data[k]
+
+    #####
+    def __getitem__(self,k, tol=1e-3*u.AA):
+        ''' Passback a row of data on the input line
+        Paramaters:
+        ----------
+        k: overloaded
+          float,Quantity -- Wavelength
+          str -- Name
+
+        Returns:
+        ----------
+        Astropy Row from the data table
+        '''
+        if type(k) in [float,Quantity]: # Wavelength
+            mt = np.where( np.abs(k-self.wrest) < tol)[0]
+        elif type(k) in [str]: # Name
+            mt = np.where( k == self.name )[0]
+        else:
+            raise ValueError('Not prepare for this type')
+
+        # Matches
+        if len(mt) == 0:
+            print('No such line in the list')
+            return None
+        elif len(mt) == 1:
+            return self._data[mt][0]  # Pass back as a Row not a Table
+        else:
+            raise ValueError('Multiple lines in the list')
+
+
+    # Printing
+    def __repr__(self):
+        # Generate sets string
+        for kk,llist in enumerate(self.lists):
+            if kk == 0:
+                sstr = llist
+            else:
+                sstr = sstr + ',' + llist
+        return '[LineList: {:s}]'.format(sstr)
--- linetools/lists/parse.py
+++ linetools/lists/parse.py
@@ -10,9 +10,9 @@ import os, imp
 from astropy import units as u
 from astropy import constants as const
 from astropy.io import fits
-from astropy.tables import QTable
+from astropy.table import QTable, Column, Table
 
-#from xastropy.xutils import xdebug as xdb
+from xastropy.xutils import xdebug as xdb
 lt_path = imp.find_module('linetools')[1]
 
 #
@@ -40,28 +40,167 @@ def line_data():
         'wrest': 0.*u.AA,     # Rest Wavelength (Quantity)
         'f':  0.,             # Oscillator strength
         'gk': 0.,             # Degeneracy of the upper level
-        'Ej': 0.*u.eV,        # Energy of lower level (relative to ground state)
-        'Ek': 0.*u.eV,        # Energy of upper level (relative to ground state)
-        'Ex': 0./u.cm         # Excitation energy (cm^-1)
+        'Ej': 0./u.cm,        # Energy of lower level (relative to ground state)
+        'Ek': 0./u.cm,        # Energy of upper level (relative to ground state)
+        'Ex': 0./u.cm,        # Excitation energy (cm^-1)
         'A': 0./u.s,          # Einstein coefficient
-        'gamma': 0.,          # Sum of A
+        'gj': 0,              # Lower statistical weight (2J+1)
+        'gk': 0,              # Upper statistical weight (2J+1)
+        'gamma': 0./u.s,      # Sum of A
         'nj': 0,              # Orbital level of lower state (or vibrational level)
         'nk': 0,              # Orbital level of upper state (or vibrational level)
         'Jj': 0.,             # Tot ang mom (z projection) of lower state (or rotation level)
         'Jk': 0.,             # Tot ang mom (z projection) of upper state (or rotation level)
+        'el': 0,              # Electronic transition (2=Lyman (B-X), 3=Werner (C-X)) 
         'Z': 0,               # Atomic number (for atoms)
         'ion': 0,             # Ionic state
+        'mol': '',            # Molecular name (H2, HD, CO, C13O)
+        'Ref': '',            # References
         'group': 0            # Flag for grouping
         }
+
+    return ldict
 
 #
 def read_H2():
     ''' Simple def to read H2 data
+
     Returns:
     --------
     QTable of H2 lines
     '''
-    H2_fil = 
-    data = QTable.read(gdfil, guess=False, comment=';')
+    H2_fil = lt_path + '/data/lines/H2_resonance.ascii'
+    print('linetools.lists.parse: Reading linelist --- \n   {:s}'.format(H2_fil))
+    data = Table.read(H2_fil, format='ascii', guess=False, comment=';')
+
+    # Units
+    data['wrest'].unit = u.AA
+
+    # Rename some columns
+    data.rename_column('Jp', 'Jj')
+    data.rename_column('Jpp', 'Jk')
+    data.rename_column('np', 'nj')
+    data.rename_column('npp', 'nk')
+
+    # Molecule column
+    cmol = Column(['H2']*len(data), name='mol')
+    data.add_column(cmol)
+
+    # Group
+    cgroup = Column(np.ones(len(data),dtype='int')*(2**3), name='group')
+    data.add_column(cgroup)
+
+    # Return
+    return data
+
+#
+def parse_morton03():
+    '''Parse tables from Morton 2003, ApJS, 149, 205 
+    '''
+    ## Read Table 2
+    morton03_tab2 = lt_path + '/data/lines/morton03_table2.dat'
+    f = open(morton03_tab2, 'r')
+    lines = f.readlines()
+    f.close()
+
+    ## Find Elements and Ions
+    elmi = []
+    elmZ = []
+    elmc = []
+    ioni = []
+    ionv = []
+    for kk,line in enumerate(lines):
+        if 'Z = ' in line:
+            # Grab Z
+            ipos = line.find('Z = ')
+            elmZ.append(int(line[ipos+4:ipos+7]))
+            elmc.append(line[ipos-5:ipos-1].strip())
+            # Line index
+            elmi.append(kk)
+        if 'IP = ' in line:
+            # Grab Z
+            ipos = line.find(' ')
+            ionv.append(line[ipos+1:ipos+5].strip())
+            # Line index
+            ioni.append(kk)
+
+    ## Initialize Dicts
+    ldict = line_data()
+    ldict['Ref'] = 'Morton2003'
+    all_dict = [ldict]*len(lines)
+
+    ## Parse lines with UV rest wavelength
+    count = 0
+    for kk,line in enumerate(lines):
+        try:
+            tmp = line[23] == '.'
+        except IndexError:
+            pass
+        else:
+            if tmp: # UV wavelength?
+                # Parse
+                # Wavelength
+                all_dict[count]['wrest'] = float(line[19:28]) * u.AA
+                # Z
+                gdZ = np.where( (kk > np.array(elmi)) & (kk < np.roll(np.array(elmi),1)))[0]
+                if len(gdZ) != 1:
+                    raise ValueError('Uh oh')
+                all_dict[count]['Z'] = elmZ[gdZ]
+                # ion
+                gdi = np.where( (kk > np.array(ioni)) & (kk < np.roll(np.array(ioni),1)))[0]
+                if len(gdi) != 1:
+                    raise ValueError('Uh oh')
+                all_dict[count]['ion'] = roman_to_number(ionv[gdi])
+                # Name
+                all_dict[count]['name'] = elmc[gdZ]+ionv[gdi]+' {:d}'.format(
+                    int(all_dict[count]['wrest'].value))
+                # f
+                all_dict[count]['f'] = float(line[79:89])
+                # Ej, Ek
+                all_dict[count]['Ej'] = float(line[29:38]) / u.cm
+                all_dict[count]['Ek'] = float(line[40:50]) / u.cm
+                # A
+                all_dict[count]['A'] = float(line[59:68]) / u.s
+                # gamma
+                try:
+                    all_dict[count]['gamma'] = float(line[69:79]) / u.s
+                except ValueError:
+                    pass
+                # gl, gu
+                all_dict[count]['gj'] = int(line[52:54])
+                all_dict[count]['gk'] = int(line[56:58])           
+
+                # Check
+                print(all_dict[count])
+                xdb.set_trace()
+
+                # Increment
+                count += 1
+    #
+def roman_to_number(val):
+    '''Convert simple Roman numerals to Arabic
+
+    Parameters:
+    -------------
+    val: str or unicoce
+      Roman numeral for conversion
+    Returns:
+    ------------
+    Number
+    '''
+    if val.strip() == 'I':
+        return 1
+    elif val.strip() == 'II':
+        return 2
+    elif val.strip() == 'III':
+        return 3
+    elif val.strip() == 'IV':
+        return 4
+    elif val.strip() == 'V':
+        return 5
+    elif val.strip() == 'VI':
+        return 6
+    else:
+        raise ValueError('Not setup for {:s}'.format(val)) 
 
 ###########################################
