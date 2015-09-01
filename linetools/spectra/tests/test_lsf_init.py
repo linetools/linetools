c2c637c6ba059a4430720871b367a72d5afe5098
Nicolas Tejos <ntejos@gmail.com>
2015-09-01 10:56:08 -0700

    LSF: COS initialization test added.

--- linetools/spectra/tests/test_lsf_init.py
+++ linetools/spectra/tests/test_lsf_init.py
@@ -10,11 +10,26 @@ def test_lsf_COS():
     wv_array = np.arange(1200,1500,1)*u.AA
     
     gratings = ['G130M','G160M', 'G140L','G230L', 'G185M', 'G225M', 'G285M']
-    life_positions = ['1']
-    
+    life_positions = ['1','2']
+    cen_waves_G160M = ['1577','1589','1600','1611','1623']
+    cen_waves_G130M = ['1291','1300','1309','1318','1327']
+
     for grating in gratings:
         for lp in life_positions:
-            print(grating,lp)
+            
             instr_config = dict(name='COS',grating=grating,life_position=lp)
-            lsf = LSF(wv_array,instr_config)
+            if lp == '2':
+            	if grating not in ['G130M','G160M']:
+            		continue
+            	if grating == 'G130M':
+            		cen_waves_aux = cen_waves_G130M
+            	elif grating == 'G160M':
+            		cen_waves_aux = cen_waves_G160M
 
+            	for cen_wave in cen_waves_aux:
+            		instr_config['cen_wave'] = cen_wave
+            		lsf = LSF(wv_array,instr_config)
+            		print(lp,grating,cen_wave)
+            elif lp == '1':
+				lsf = LSF(wv_array,instr_config)
+				print(lp,grating)
