3530a846f0e55b933020599404c02759dad4167b
J. Xavier Prochaska <xavier@ucolick.org>
2015-11-25 08:15:41 -0800

    AbsSys begins

---
+++ docs/abssys.rst
@@ -0,0 +1,65 @@
+.. highlight:: rest
+
+******************
+AbsSystem Class
+******************
+
+.. index:: AbsSystem
+
+Overview
+========
+
+This Class is designed to organize and analyze an
+absorption system.  This is generally constructed
+of one or more (:ref:`AbsComponent Class`).
+The base class is abstract, i.e. one must instantiate
+one of its flavors (e.g. HILyman, MgII, LLS, DLA).
+
+By definition, an AbsSystem is a unique collection of
+absorption components.  It is specified by:
+
+=============== ========   ============== ============================================
+Property        Variable   Type           Description
+=============== ========   ============== ============================================
+RA, DEC         radec      tuple or coord RA,DEC in deg or astropy.coordinate
+Redshift        z          float          absorption redshift
+Velocity limits vlim       Quantity array -/+ velocity limits of the component
+=============== ========   ============== ============================================
+
+
+Instantiation
+-------------
+
+The AbsSystem Class may be instantiated in a few ways.
+The default sets the properties listed above::
+
+	gensys = GenericAbsSystem((15.23*u.deg,-23.22*u.deg), 1.244, [-500,500]*u.km/u.s, NHI=16.)
+
+More commonly, one will instantiate with one or more AbsComponent objects::
+
+    # HI Lya, Lyb
+    lya = AbsLine(1215.670*u.AA)
+    lya.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lya.attrib['z'] = 2.92939
+    lyb = AbsLine(1025.7222*u.AA)
+    lyb.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lyb.attrib['z'] = lya.attrib['z']
+    abscomp = AbsComponent.from_abslines([lya,lyb])
+    abscomp.coord = radec
+    # Finish
+    HIsys = LymanAbsSystem.from_components([abscomp])
+
+
+::::
+
+Attributes
+----------
+
+Plots
+-----
+
+Methods
+-------
+
+Output
+------
---
+++ linetools/isgm/tests/test_init_abssys.py
@@ -0,0 +1,71 @@
+# Module to run tests on generating AbsSystem
+
+from __future__ import print_function, absolute_import, division, unicode_literals
+
+# TEST_UNICODE_LITERALS
+
+import pytest
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+import numpy as np
+
+from linetools.isgm.abscomponent import AbsComponent
+from linetools.isgm.abssystem import GenericAbsSystem, LymanAbsSystem
+from linetools.spectralline import AbsLine
+
+import pdb
+
+def test_init():
+    # Simple properties
+    radec = SkyCoord(ra=123.1143*u.deg, dec=-12.4321*u.deg)
+    gensys = GenericAbsSystem(radec, 1.244, [-500,500]*u.km/u.s, NHI=16.)
+    # Test
+    assert gensys.abs_type == 'Generic'
+    np.testing.assert_allclose(gensys.zabs,1.244)
+
+def test_one_component():
+    radec = SkyCoord(ra=123.1143*u.deg, dec=-12.4321*u.deg)
+    # HI Lya, Lyb
+    lya = AbsLine(1215.670*u.AA)
+    lya.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lya.attrib['z'] = 2.92939
+    lyb = AbsLine(1025.7222*u.AA)
+    lyb.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lyb.attrib['z'] = lya.attrib['z']
+    abscomp = AbsComponent.from_abslines([lya,lyb])
+    abscomp.coord = radec
+    # Instantiate
+    HIsys = LymanAbsSystem.from_components([abscomp])
+    # Test
+    assert HIsys.abs_type == 'HILyman'
+    assert len(HIsys._components) == 1
+    assert HIsys._components[0].Zion[0] == 1
+    assert HIsys._components[0].Zion[1] == 1
+
+def test_multi_components():
+    radec = SkyCoord(ra=123.1143*u.deg, dec=-12.4321*u.deg)
+    # HI Lya, Lyb
+    lya = AbsLine(1215.670*u.AA)
+    lya.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lya.attrib['z'] = 2.92939
+    lyb = AbsLine(1025.7222*u.AA)
+    lyb.analy['vlim'] = [-300.,300.]*u.km/u.s
+    lyb.attrib['z'] = lya.attrib['z']
+    abscomp = AbsComponent.from_abslines([lya,lyb])
+    abscomp.coord = radec
+    # SiII
+    SiIItrans = ['SiII 1260', 'SiII 1304', 'SiII 1526', 'SiII 1808']
+    abslines = []
+    for trans in SiIItrans:
+        iline = AbsLine(trans)
+        iline.attrib['z'] = 2.92939
+        iline.analy['vlim'] = [-250.,80.]*u.km/u.s
+        abslines.append(iline)
+    #
+    SiII_comp = AbsComponent.from_abslines(abslines)
+    SiII_comp.coord = radec
+    # Instantiate
+    LLSsys = GenericAbsSystem.from_components([abscomp,SiII_comp])
+    # Test
+    assert len(LLSsys._components) == 2
+
