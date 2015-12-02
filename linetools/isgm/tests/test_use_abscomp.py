179a7568c0760ec82539f3b3d4c7a06c45700c98
Nicolas Tejos <ntejos@gmail.com>
2015-12-02 13:49:34 -0800

    abscomponent: error fixed

--- linetools/isgm/tests/test_use_abscomp.py
+++ linetools/isgm/tests/test_use_abscomp.py
@@ -128,18 +128,21 @@ def test_repr_vpfit():
     s = abscomp.repr_vpfit(tie_strs=('a', 'b', 'CD'),fix_strs=('', 'f', ''))
     assert s == 'HI 2.92939a 0.00000 10.00F 0.00 0.00cd 0.00! Something\n'
 
+    abscomp, SiIIlines = mk_comp('SiII')
+    s = abscomp.repr_vpfit()
+    assert s == 'SiII 2.92939 0.00000 10.00 0.00 0.00 0.00\n'
 
 def test_repr_alis():
     abscomp, HIlines = mk_comp('HI')
     s = abscomp.repr_alis()
-    assert s == 'voigt   ion=1H_I 0.00 redshift=2.92939 0.00 1.0E+04\n'
+    assert s == 'voigt   ion=1H_I 0.00 redshift=2.92939 0.0 1.0E+04\n'
 
     abscomp, SiIIlines = mk_comp('SiII')
     s = abscomp.repr_alis(T_kin=10**5*u.K)
-    assert s == 'voigt   ion=28Si_II 0.00 redshift=2.92939 0.00 1.0E+05\n'
+    assert s == 'voigt   ion=28Si_II 0.00 redshift=2.92939 0.0 1.0E+05\n'
 
     abscomp.comment = 'Something'
     s = abscomp.repr_alis()
-    assert s == 'voigt   ion=28Si_II 0.00 redshift=2.92939 0.00 1.0E+04# Something\n'
+    assert s == 'voigt   ion=28Si_II 0.00 redshift=2.92939 0.0 1.0E+04# Something\n'
     s = abscomp.repr_alis(tie_strs=('a', 'b', 'CD',''),fix_strs=('', 'f', '', ''))
-    assert s == 'voigt   ion=28Si_II 0.00a redshift=2.92939F 0.00cd 1.0E+04# Something\n'
+    assert s == 'voigt   ion=28Si_II 0.00a redshift=2.92939F 0.0cd 1.0E+04# Something\n'
