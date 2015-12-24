2122a7de27e1585af8691a88b15bfdc62aa4671a
J. Xavier Prochaska <xavier@ucolick.org>
2015-12-24 06:16:24 -0800

    with a test

--- linetools/tests/test_utils.py
+++ linetools/tests/test_utils.py
@@ -1,9 +1,17 @@
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-from ..utils import between
+from ..utils import between, radec_to_coord
 import numpy as np
+import pdb
 
 def test_between():
     x = [1,2,3,4,5]
     c0 = between(x, 2.5, 3.5)
     assert np.all(c0 == np.array([False, False, True, False, False]))
+
+def test_radeccoord():
+    for radec in ['J124511+144523', '124511+144523',
+                  'J12:45:11+14:45:23', ('12:45:11','+14:45:23')]:
+        coord = radec_to_coord(radec)
+        # Test
+        np.testing.assert_allclose(coord.ra.value, 191.2958333333333)
