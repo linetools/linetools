3c30bacb83938afac404dba9c939cec3a5805397
Neil Crighton <neilcrighton@gmail.com>
2016-01-20 13:58:54 +1100

    fix for lt_plot

--- linetools/analysis/interactive_plot.py
+++ linetools/analysis/interactive_plot.py
@@ -68,9 +68,9 @@ S,U          Smooth/unsmooth spectrum
 """
 
     def __init__(self):
-        # Navigate dict
-        self.nav_dict = dict(nav=ltgu.navigate(init=True))
-        pass
+        """ Initialize parameters for plotting the spectrum
+        """
+        self.nav_dict = dict(nav=ltgu.navigate(0, 0, init=True))
 
     def on_keypress_navigate(self, event):
         """ Process a keypress event. Requires attributes self.ax,
@@ -79,66 +79,9 @@ S,U          Smooth/unsmooth spectrum
         # Requiring inaxes for all of these now
         if (event.key in self.nav_dict['nav']) and event.inaxes:
             ltgu.navigate(self.nav_dict, event)
-            self.ax.set_xlim(self.nav_dict['xmnx'])
-            self.ax.set_ylim(self.nav_dict['ymnx'])
+            self.ax.set_xlim(self.nav_dict['x_minmax'])
+            self.ax.set_ylim(self.nav_dict['y_minmax'])
             self.fig.canvas.draw()
-        """
-        # Navigation
-        if event.key == 'i' and event.inaxes:
-            x0,x1 = self.ax.get_xlim()
-            x = event.xdata
-            dx = abs(x1 - x0)
-            self.ax.set_xlim(x - 0.275*dx, x + 0.275*dx)
-            self.fig.canvas.draw()
-        elif event.key == 'o' and event.inaxes:
-            x0,x1 = self.ax.get_xlim()
-            x = event.xdata
-            dx = abs(x1 - x0)
-            self.ax.set_xlim(x - 0.95*dx, x + 0.95*dx)
-            self.fig.canvas.draw()
-        elif event.key == 'Y' and event.inaxes:
-            y0,y1 = self.ax.get_ylim()
-            y = event.ydata
-            dy = abs(y1 - y0)
-            self.ax.set_ylim(y0 - 0.05*dy, y1 + 0.4*dy)
-            self.fig.canvas.draw()
-        elif event.key == 'y' and event.inaxes:
-            x0,x1 = self.ax.get_xlim()
-            y0,y1 = get_flux_plotrange(self.fl[between(self.wa, x0, x1)])
-            self.ax.set_ylim(y0, y1)
-            self.fig.canvas.draw()
-        elif event.key == ']':
-            x0,x1 = self.ax.get_xlim()
-            dx = abs(x1 - x0)
-            self.ax.set_xlim(x1 - 0.1*dx, x1 + 0.9*dx)
-            self.fig.canvas.draw()
-        elif event.key == '[':
-            x0,x1 = self.ax.get_xlim()
-            dx = abs(x1 - x0)
-            self.ax.set_xlim(x0 - 0.9*dx, x0 + 0.1*dx)
-            self.fig.canvas.draw()
-        elif event.key == 'w':
-            self.ax.set_xlim(self.wa[0], self.wa[-1])
-            y0,y1 = get_flux_plotrange(self.fl)
-            self.ax.set_ylim(y0, y1)
-            self.fig.canvas.draw()
-        elif event.key == 'b' and event.inaxes:
-            y0, y1 = self.ax.get_ylim()
-            self.ax.set_ylim(event.ydata, y1)
-            self.fig.canvas.draw()
-        elif event.key == 't' and event.inaxes:
-            y0, y1 = self.ax.get_ylim()
-            self.ax.set_ylim(y0, event.ydata)
-            self.fig.canvas.draw()
-        elif event.key == 'l' and event.inaxes:
-            x0, x1 = self.ax.get_xlim()
-            self.ax.set_xlim(event.xdata, x1)
-            self.fig.canvas.draw()
-        elif event.key == 'r' and event.inaxes:
-            x0, x1 = self.ax.get_xlim()
-            self.ax.set_xlim(x0, event.xdata)
-            self.fig.canvas.draw()
-        """
 
     def on_keypress_smooth(self, event):
         """ Smooth the flux with a gaussian. Requires attributes
@@ -178,18 +121,27 @@ class PlotWrapNav(PlotWrapBase):
       Whether to print a help message when first called.
     """
     def __init__(self, fig, ax, wa, fl, artists, printhelp=True):
-        self.artists = artists
-        self.fig = fig
-        self.ax = ax
-        self.nav_dict = dict(nav=ltgu.navigate(0, 0, init=True))
-        self.nav_dict['xmnx'] = self.ax.get_xlim()
-        self.nav_dict['ymnx'] = self.ax.get_ylim()
+
+
+        super(PlotWrapNav, self).__init__()
+
         if isinstance(wa, u.Quantity):
             wa = wa.value
         self.wa = wa
         if isinstance(fl, u.Quantity):
             fl = fl.value
         self.fl = fl
+
+        xmin = np.min(self.wa)
+        xmax = np.max(self.wa)
+        ymin, ymax = get_flux_plotrange(self.fl)
+        self.nav_dict['x_minmax'] = np.array([xmin, xmax])
+        self.nav_dict['y_minmax'] = [ymin, ymax]
+
+        self.artists = artists
+        self.fig = fig
+        self.ax = ax
+
         self.nsmooth = 0
         self.last_keypress = None
         # disable existing keypress events (like 's' for save).
