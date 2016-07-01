54002217a0949d130b132a51130e166d1d82d307
J. Xavier Prochaska <xavier@ucolick.org>
2016-07-01 03:33:49 -0700

    script improvements

--- linetools/scripts/lt_line.py
+++ linetools/scripts/lt_line.py
@@ -24,9 +24,9 @@ def parser(options=None):
         description='Print spectral line data of a line or lines.')
     parser.add_argument("inp", nargs='?', default=None, help="Ion, transition name, or Rest wavelength (e.g. HI, HI1215 or 1215)")
     parser.add_argument("-a", "--all", default=False, action='store_true', help="Print all lines")
-    parser.add_argument("--llist", default='ISM', action='store_true', help="Name of LineList:  ISM, HI, H2, CO, etc.")
-    parser.add_argument("-t", "--toler", default=1., action='store_true', help="Matching tolerance (in Ang) on input wavelength")
-    parser.add_argument("-z", "--redshift", default=0., help="Matching tolerance (in Ang) on input wavelength")
+    parser.add_argument("--llist", default='ISM', type=str, help="Name of LineList:  ISM, HI, H2, CO, etc.")
+    parser.add_argument("-t", "--toler", default=1., type=float, help="Matching tolerance (in Ang) on input wavelength")
+    parser.add_argument("-z", "--redshift", default=0., type=float, help="Matching tolerance (in Ang) on input wavelength")
     #parser.add_argument("wave", type=str, default=None, help="Rest wavelength in Angstroms")
 
     if options is None:
@@ -46,7 +46,12 @@ def main(args=None):
     from linetools.lists.linelist import LineList
     from linetools.abund import ions as ltai
     import numpy as np
-    cols = ['name', 'wrest', 'f', 'A']
+    if pargs.llist in ['CO']:
+        cols = ['mol', 'label', 'wrest', 'f']
+    elif pargs.llist in ['H2']:
+        cols = ['mol', 'name', 'wrest', 'f']
+    else:
+        cols = ['name', 'wrest', 'f', 'A']
     # LineList
     llist = LineList(pargs.llist)
     # Redshift?
@@ -56,7 +61,10 @@ def main(args=None):
         cols += ['z']
     # All?
     if pargs.all:
-        llist._data[cols].pprint(99999)
+        try:
+            llist._data[cols].pprint(99999)
+        except ValueError:
+            pdb.set_trace()
         return
     # Grab line(s)
     if ustr(pargs.inp[0]).isdecimal():  # Input rest wavelength
