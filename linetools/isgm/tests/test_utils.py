491c1e6401de43aa2ec4d79e5588b3442e4757c5
Nicolas Tejos <ntejos@gmail.com>
2016-07-20 17:10:53 -0400

    abscomponent: typo_bug

--- linetools/isgm/tests/test_utils.py
+++ linetools/isgm/tests/test_utils.py
@@ -12,33 +12,33 @@ import imp
 lt_path = imp.find_module('linetools')[1]
 
 
-def test_overlaping_chunks():
+def test_overlapping_chunks():
     chunk1 = (1,2,3,4)
     chunk2 = [3,4,5,6]
-    t = ltiu.overlaping_chunks(chunk1, chunk2)
+    t = ltiu.overlapping_chunks(chunk1, chunk2)
     assert t
-    t = ltiu.overlaping_chunks(chunk2, chunk1)
+    t = ltiu.overlapping_chunks(chunk2, chunk1)
     assert t
 
     chunk2 = np.array([5,7])
-    f = ltiu.overlaping_chunks(chunk2, chunk1)
+    f = ltiu.overlapping_chunks(chunk2, chunk1)
     assert ~f
-    f = ltiu.overlaping_chunks(chunk2*u.AA, chunk1*u.AA)
+    f = ltiu.overlapping_chunks(chunk2*u.AA, chunk1*u.AA)
     assert ~f
 
     # Wrong format
     try:
-        f = ltiu.overlaping_chunks(chunk2*u.AA, chunk1)
+        f = ltiu.overlapping_chunks(chunk2*u.AA, chunk1)
     except ValueError:
         pass
     try:
-        f = ltiu.overlaping_chunks(chunk2*u.AA, chunk1*u.K)
+        f = ltiu.overlapping_chunks(chunk2*u.AA, chunk1*u.K)
     except ValueError:
         pass
 
     # not sorted
     try:
-        f = ltiu.overlaping_chunks((1,2,3,4), (4,5,3,6))
+        f = ltiu.overlapping_chunks((1,2,3,4), (4,5,3,6))
     except ValueError:
         pass
 
