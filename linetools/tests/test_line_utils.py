e27d7a32ae5157a335054bb397684bcd5189024c
Nicolas Tejos <ntejos@gmail.com>
2016-08-03 13:52:17 -0400

    refactor_vlim: test_coincident_line() passing.

--- linetools/tests/test_line_utils.py
+++ linetools/tests/test_line_utils.py
@@ -43,27 +43,28 @@ def test_transtabl():
 
 
 def test_coincident_line():
-    # TODO FOR TEJOS TO FIX
     # Init AbsLines
     line1 = AbsLine(1215.6700*u.AA)
     line2 = AbsLine('CII 1334')
+
     # expected errors
+    # no limits set
     try:
         answer = line1.coincident_line(line2)
     except ValueError:
         pass
-    #line1.analy['wvlim'] = (1500,1510)*u.AA
+    # limit only for l1
     line1.limits.set((1500,1510)*u.AA)
     try:
         answer = line1.coincident_line(line2)
     except ValueError:
         pass
-    line2.analy['wvlim'] = (1509,1520)*u.AA
-
+    # now limit for l2
+    line2.limits.set((1509,1520)*u.AA)
     # expected overlap
     assert line1.coincident_line(line2)
     assert line2.coincident_line(line1)
     # not expected overlap
-    line2.analy['wvlim'] = (1510.1,1520)*u.AA
+    line2.limits.set((1510.1,1520)*u.AA)
     assert not line1.coincident_line(line2)
     assert not line2.coincident_line(line1)
