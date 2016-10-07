a9881de6d932e77d03a32ae656373a5a9ff986ec
J. Xavier Prochaska <xavier@ucolick.org>
2016-10-07 06:11:09 -0700

    another fix in dN/dX

---
+++ linetools/scripts/lt_radec.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""
+Ingest input coordinates and print them to the screen
+Plot the data for a line (or lines)
+Also print the line data
+  Examples:
+  lt_radec 152.25900,7.22885
+"""
+from __future__ import (print_function, absolute_import, division, unicode_literals)
+
+import pdb
+
+try:
+    ustr = unicode
+except NameError:
+    ustr = str
+
+def parser(options=None):
+    import argparse
+    # Parse
+    parser = argparse.ArgumentParser(
+        description='Print coordinates in several formats from input one.')
+    parser.add_argument("inp", nargs='?', default=None, help="RA,DEC (e.g. 152.25900,7.22885), JXX (e.g. J100902.16+071343.8)")
+    parser.add_argument("--epoch", default=2000., type=float, help="Epoch [Not functional]")
+    #parser.add_argument("wave", type=str, default=None, help="Rest wavelength in Angstroms")
+
+    if options is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(options)
+    return args
+
+def main(args=None):
+    pargs = parser(options=args)
+    if pargs.inp is None and pargs.all is False:
+        print("No option selected.  Use -h for Help")
+        return
+    # Setup
+    from linetools import utils as ltu
+    from astropy import units as u
+    # RA, DEC?
+    if ',' in pargs.inp:
+        ra,dec = [float(ii) for ii in pargs.inp.split(',')]
+        coord = ltu.radec_to_coord((ra,dec))
+    else:
+        coord = ltu.radec_to_coord(pargs.inp)
+
+    # Time to print
+    print('J{:s}{:s}'.format(coord.ra.to_string(unit=u.hour,sep='',pad=True),
+                             coord.dec.to_string(sep='',pad=True,alwayssign=True)))
+    print('   RA={:f} deg, DEC={:f} deg'.format(coord.ra.deg, coord.dec.deg))
+
+
+if __name__ == '__main__':
+    main()
