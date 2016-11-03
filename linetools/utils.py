7b56f228e8867549506de48e016d199cfdd2bbd8
J. Xavier Prochaska <xavier@ucolick.org>
2016-11-03 09:31:47 -0700

    easier to read JSON file

--- linetools/utils.py
+++ linetools/utils.py
@@ -201,17 +201,44 @@ def jsonify(obj, debug=False):
     return obj
 
 
-def savejson(filename, obj, overwrite=False, indent=None):
-    """ Save a python object to filename using using the JSON encoder."""
+def savejson(filename, obj, overwrite=False, indent=None, easy_to_read=False,
+             **kwargs):
+    """ Save a python object to filename using using the JSON encoder.
+
+    Parameters
+    ----------
+    filename : str
+    obj : object
+      Frequently a dict
+    overwrite : bool, optional
+    indent : int, optional
+      Input to json.dump
+    easy_to_read : bool, optional
+      Another approach and obj must be a dict
+    kwargs : optional
+      Passed to json.dump
+
+    Returns
+    -------
+
+    """
+    import io
 
     if os.path.lexists(filename) and not overwrite:
         raise IOError('%s exists' % filename)
-    if filename.endswith('.gz'):
-        with gzip.open(filename, 'wt') as fh:
-            json.dump(obj, fh, indent=indent)
+    if easy_to_read:
+        if not isinstance(obj, dict):
+            raise IOError("This approach requires obj to be a dict")
+        with io.open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(obj, sort_keys=True, indent=4,
+                               separators=(',', ': '), **kwargs))
     else:
-        with open(filename, 'wt') as fh:
-            json.dump(obj, fh, indent=indent)
+        if filename.endswith('.gz'):
+            with gzip.open(filename, 'wt') as fh:
+                json.dump(obj, fh, indent=indent, **kwargs)
+        else:
+            with open(filename, 'wt') as fh:
+                json.dump(obj, fh, indent=indent, **kwargs)
 
 
 def loadjson(filename):
