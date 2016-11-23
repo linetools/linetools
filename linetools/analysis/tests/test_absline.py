0d9b1b559319d5154c11bb6af6426e900ef94835
Nicolas Tejos <ntejos@gmail.com>
2016-11-23 09:42:03 -0300

    Test error for Wr_from_N_b_transition()

--- linetools/analysis/tests/test_absline.py
+++ linetools/analysis/tests/test_absline.py
@@ -113,5 +113,6 @@ def test_Wr_from_N_b_transition():
     # test float like input for logN
     Wr = Wr_from_N_b_transition(N[0], b[0], transition='HI 1215')
     np.testing.assert_allclose(Wr, Wr_test[0], rtol=1e-5)
+    # test expected error
     with pytest.raises(ValueError):
         Wr = Wr_from_N_b_transition(N[0], b[0], transition='Wrong name jojo')
