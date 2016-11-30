be583d738a2eb9c2c1b74ad55e7e530e4c7d73f7
Nicolas Tejos <ntejos@gmail.com>
2016-11-30 13:26:45 -0300

    tests added to dvdz, and some cleaning to test_utils.

--- linetools/tests/test_utils.py
+++ linetools/tests/test_utils.py
@@ -5,9 +5,6 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 # TEST_UNICODE_LITERALS
 
 import pytest
-from ..utils import between, v_from_z, savejson, loadjson, z_from_v
-from ..utils import radec_to_coord, convert_quantity_in_dict
-from ..utils import name_from_coord
 import numpy as np
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -15,10 +12,10 @@ import linetools.utils as ltu
 
 def test_name_from_coord():
     coord = SkyCoord(ra=15.4, dec=-23.1, unit='deg')
-    name = name_from_coord(coord)
+    name = ltu.name_from_coord(coord)
     assert name == 'J010136.00-230600.0'
     # Name 2
-    name2 = name_from_coord(coord, precision=(0,0))
+    name2 = ltu.name_from_coord(coord, precision=(0,0))
     assert name2 == 'J010136-230600'
 
 
@@ -27,50 +24,60 @@ def test_convert_qdict():
     time = dict(unit='s', value=100.)
     mass = dict(unit='g', value=10.)
     idict = dict(vlim=vlim, nest=dict(time=time, mass=mass))
-    newdict = convert_quantity_in_dict(idict)
+    newdict = ltu.convert_quantity_in_dict(idict)
     assert newdict['nest']['mass'] == 10*u.g
     # Simple one
-    obj = convert_quantity_in_dict(vlim)
+    obj = ltu.convert_quantity_in_dict(vlim)
     assert obj.unit == u.km/u.s
+
 
 def test_between():
     x = [1,2,3,4,5]
-    c0 = between(x, 2.5, 3.5)
+    c0 = ltu.between(x, 2.5, 3.5)
     assert np.all(c0 == np.array([False, False, True, False, False]))
 
-def test_zfromv():
-    z = z_from_v(2., 1000.)
+
+def test_dz_from_dv():
+    dz = ltu.dz_from_dv(1000.*u.km/u.s, 2.)
+    np.testing.assert_allclose(dz, 0.0100236684175417)
+
+
+def test_z_from_dv():
+    z = ltu.z_from_dv(1000.*u.km/u.s, 2.)
     #
     np.testing.assert_allclose(z, 2.0100236684175417)
 
-def test_vfromz():
-    v = v_from_z(2., 2.1)
+
+
+def test_dv_from_z():
+    dv = ltu.dv_from_z(2.1, 2.)
     #
-    assert v.unit == u.km/u.s
-    np.testing.assert_allclose(v.value, -9826.62006340679)
+    assert dv.unit == u.km/u.s
+    np.testing.assert_allclose(dv.value, 9826.620063406788, rtol=1e-6)
+
 
 def test_save_load_json():
     tmp_dict = dict(a=1, b=2, c='adsf')
     # Write
-    savejson('tmp.json', tmp_dict, overwrite=True)
+    ltu.savejson('tmp.json', tmp_dict, overwrite=True)
     # Load
-    new_dict = loadjson('tmp.json')
+    new_dict = ltu.loadjson('tmp.json')
     assert new_dict['a'] == 1
     # Write with gzip
-    savejson('tmp.json.gz', tmp_dict, overwrite=True)
+    ltu.savejson('tmp.json.gz', tmp_dict, overwrite=True)
     # Load
-    new_dict = loadjson('tmp.json.gz')
+    new_dict = ltu.loadjson('tmp.json.gz')
     assert new_dict['a'] == 1
     # Write with easy to read
-    savejson('tmp2.json', tmp_dict, overwrite=True, easy_to_read=True)
-    new_dict2 = loadjson('tmp2.json')
+    ltu.savejson('tmp2.json', tmp_dict, overwrite=True, easy_to_read=True)
+    new_dict2 = ltu.loadjson('tmp2.json')
     assert new_dict2['a'] == 1
 
 def test_radeccoord():
     for radec in ['J124511+144523', '124511+144523',
                   'J12:45:11+14:45:23', ('12:45:11','+14:45:23'),
                   ('12:45:11', '14:45:23'), ('12 45 11','+14 45 23')]:
-        coord = radec_to_coord(radec)
+        coord = ltu.radec_to_coord(radec)
         # Test
         np.testing.assert_allclose(coord.ra.value, 191.2958333333333)
 
