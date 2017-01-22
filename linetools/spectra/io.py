c79f30996dbd76167989cb75368bd2db5b057a87
J. Xavier Prochaska <xavier@ucolick.org>
2017-01-22 07:52:05 -0800

    allow hdf5 to stay open

--- linetools/spectra/io.py
+++ linetools/spectra/io.py
@@ -623,7 +623,7 @@ def parse_linetools_spectrum_format(hdulist):
     return xspec1d
 
 
-def parse_hdf5(inp, **kwargs):
+def parse_hdf5(inp, close=True, **kwargs):
     """ Read a spectrum from HDF5 written in XSpectrum1D format
     Expects:  meta, data, units
 
@@ -675,7 +675,8 @@ def parse_hdf5(inp, **kwargs):
     except (NameError, IndexError):
         co = None
     # Finish
-    hdf5.close()
+    if close:
+        hdf5.close()
     return XSpectrum1D(data['wave'], data['flux'], sig=sig, co=co,
                           meta=meta, units=units, **kwargs)
 
