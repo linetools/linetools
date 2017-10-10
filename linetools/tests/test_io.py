2188dad84b857ccf0b332f99e287a5d997dc177e
thsyu <thsyu@ucsc.edu>
2017-10-10 14:47:10 -0700

    add test on creating emline from ALIS output file

--- linetools/tests/test_io.py
+++ linetools/tests/test_io.py
@@ -16,5 +16,9 @@ def data_path(filename):
     return os.path.join(data_dir, filename)
 
 def test_emline_from_alis():
-    lio.emlines_from_alis_output(data_path('spec1d_J0018p2345_KASTb_coadd.mod.out'))
+    J0018_emlines = lio.emlines_from_alis_output(data_path('spec1d_J0018p2345_KASTb_coadd.mod.out'))
+
+    assert len(J0018_emlines) == 8
+    np.testing.assert_allclose(J0018_emlines[0].z, 0.01540425)
+    np.testing.assert_allclose(J0018_emlines[1].wrest.value, 4341.684) #Hgamma
 
