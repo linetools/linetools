61db5fa28920c076d0698702543b5a6d1545a5ab
profxj <xavier@ucolick.org>
2017-10-16 15:44:21 -0700

    still struggling

--- linetools/tests/setup_package.py
+++ linetools/tests/setup_package.py
@@ -1,4 +1,3 @@
 def get_package_data():
     return {
-        'linetools.tests': ['files/*.fits', 'files/*.dat', 'files/*.json', 'files/*.all', 'files/*.fits.gz', 'files/*.out'],
         _ASTROPY_PACKAGE_NAME_ + '.tests': ['coveragerc']}
