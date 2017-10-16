c296b4db1e4ca2e19bb1540c3879ccc6891a71b7
profxj <xavier@ucolick.org>
2017-10-16 14:40:49 -0700

    remove setup_package.py

--- linetools/tests/test_io.py
+++ linetools/tests/test_io.py
@@ -15,6 +15,7 @@ def data_path(filename):
     data_dir = os.path.join(os.path.dirname(__file__), 'files')
     return os.path.join(data_dir, filename)
 
+
 def test_emline_from_alis():
     J0018_emlines = lio.emlines_from_alis_output(data_path('spec1d_J0018p2345_KASTb_coadd.mod.out'))
 
