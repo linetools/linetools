e63cda7c39fcd5ae2bc9f094c57e285804d79025
jnburchett <jnburchett@yahoo.com>
2025-07-15 14:59:27 -0600

    kludge again

--- linetools/abund/solar.py
+++ linetools/abund/solar.py
@@ -51,7 +51,7 @@ class SolarAbund(object):
         """
         # Data file
         if self.ref == 'Asplund2009':
-            dat_file = l_path + 'linetools/data/abund/solar_Asplund2009.dat'
+            dat_file = l_path + '/linetools/data/abund/solar_Asplund2009.dat'
             # Read table
             names = ('Elm', 'Abund', 'Z')
             table = ascii.read(dat_file, format='no_header', names=names) 
